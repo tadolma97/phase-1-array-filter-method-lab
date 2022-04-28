77184819d35d5db07c6085a1bc82b8ff7dc32b77
Tashi Dolma <tadolma97@gmail.com>
2022-04-28 13:24:44 -0400

    commit tree aee2e3d25836b1df0214bf60c4d8e881bad2a606

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(star=>name.toLowerCase()===star.toLowerCase());
+}
+
+function fuzzyMatch(drivers,letter){
+    return drivers.filter(driver=>driver.charAt(0).toLowerCase()===letter.charAt(0).toLowerCase());
+}
+
+function matchName(drivers, ming){
+    return drivers.filter(driver=>driver.name===ming);
+}
